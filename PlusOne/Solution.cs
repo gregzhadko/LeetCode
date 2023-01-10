411f7e5802002a684bd43dc90d5b54c53fcd2828
Grigory Zhadko <gregzhadko@gmail.com>
2023-01-10 22:08:39 +0400

    PlusOne task is solved

---
+++ PlusOne/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace PlusOne;
+
+public class Solution
+{
+    public int[] PlusOne(int[] digits)
+    {
+        var currentIndex = digits.Length - 1;
+        while (currentIndex >= 0)
+        {
+            if (digits[currentIndex] != 9)
+            {
+                digits[currentIndex]++;
+                return digits;
+            }
+            
+            digits[currentIndex] = 0;
+            currentIndex--;
+        }
+
+        if (currentIndex == -1)
+        {
+            var newDigits = new int[digits.Length + 1];
+            digits.CopyTo(newDigits, 1);
+            newDigits[0] = 1;
+            return newDigits;
+        }
+
+        return digits;
+    }
+}
---
+++ PlusOne/Tests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+
+namespace PlusOne;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        var input = new[] { 1, 2, 3 };
+        var expected = new[] { 1, 2, 4 };
+        var actual = new Solution().PlusOne(input);
+        Assert.AreEqual(expected, actual);
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        var input = new[] { 4, 3, 2, 1 };
+        var expected = new[] { 4, 3, 2, 2 };
+        var actual = new Solution().PlusOne(input);
+        Assert.AreEqual(expected, actual);
+    }
+    
+    [Test]
+    public void Test3()
+    {
+        var input = new[] { 0 };
+        var expected = new[] { 1 };
+        var actual = new Solution().PlusOne(input);
+        Assert.AreEqual(expected, actual);
+    }
+    
+    [Test]
+    public void Test4()
+    {
+        var input = new[] { 9 };
+        var expected = new[] { 1, 0 };
+        var actual = new Solution().PlusOne(input);
+        Assert.AreEqual(expected, actual);
+    }
+    
+    [Test]
+    public void Test5()
+    {
+        var input = new[] { 9, 9 };
+        var expected = new[] { 1, 0, 0 };
+        var actual = new Solution().PlusOne(input);
+        Assert.AreEqual(expected, actual);
+    }
+    
+    [Test]
+    public void Test6()
+    {
+        var input = new[] { 9, 9, 9 };
+        var expected = new[] { 1, 0, 0, 0 };
+        var actual = new Solution().PlusOne(input);
+        Assert.AreEqual(expected, actual);
+    }
+}
