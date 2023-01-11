6c691807cb4661b70ecfd7c0b79669880a86d273
Grigory Zhadko <gregzhadko@gmail.com>
2023-01-12 01:05:59 +0400

    Swap Nodes in Pairs task

---
+++ SwapNodesInPairs/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace SwapNodesInPairs;
+
+public class Solution
+{
+    public ListNode? SwapPairs(ListNode? head)
+    {
+        if (head?.next == null)
+        {
+            return head;
+        }
+        
+        var current = head;
+        var newHead = current.next;
+        ListNode? prevNode = null;
+        while (current != null && current.next != null)
+        {
+            var next = current.next;
+            current.next = current.next.next;
+            next.next = current;
+            
+            if (prevNode != null)
+            {
+                prevNode.next = next;
+            }
+
+            prevNode = current;
+            current = current.next;
+        }
+        return newHead;
+    }
+}
---
+++ SwapNodesInPairs/Tests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+
+namespace SwapNodesInPairs;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        var head = new ListNode(1, new(2, new(3, new(4))));
+        var result = new Solution().SwapPairs(head);
+        Assert.AreEqual(2, result.val);
+        Assert.AreEqual(1, result.next.val);
+        Assert.AreEqual(4, result.next.next.val);
+        Assert.AreEqual(3, result.next.next.next.val);
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        var head = new ListNode(1, new(2, new(3, new(4, new(5)))));
+        var result = new Solution().SwapPairs(head);
+        Assert.AreEqual(2, result.val);
+        Assert.AreEqual(1, result.next.val);
+        Assert.AreEqual(4, result.next.next.val);
+        Assert.AreEqual(3, result.next.next.next.val);
+        Assert.AreEqual(5, result.next.next.next.next.val);
+    }
+    
+    [Test]
+    public void Test3()
+    {
+        var head = new ListNode(1, new(2, new(3, new(4, new(5, new(6))))));
+        var result = new Solution().SwapPairs(head);
+        Assert.AreEqual(2, result.val);
+        Assert.AreEqual(1, result.next.val);
+        Assert.AreEqual(4, result.next.next.val);
+        Assert.AreEqual(3, result.next.next.next.val);
+        Assert.AreEqual(6, result.next.next.next.next.val);
+        Assert.AreEqual(5, result.next.next.next.next.next.val);
+    }
+    
+    [Test]
+    public void Test4()
+    {
+        var head = new ListNode(1, new(2));
+        var result = new Solution().SwapPairs(head);
+        Assert.AreEqual(2, result.val);
+        Assert.AreEqual(1, result.next.val);
+    }
+}
