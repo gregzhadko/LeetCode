fa0f0ea34935ed86c151de5afaab584a7a17cd83
Grigory Zhadko <gregzhadko@gmail.com>
2023-01-16 01:59:01 +0400

    Delete Node in a linked list

---
+++ DeleteNodeInALinkedList/Solution.cs
@@ -0,0 +1,9 @@
+﻿namespace DeleteNodeInALinkedList;
+
+public class Solution
+{
+    public void Delete(Node node)
+    {
+        (node.Value, node.Next) = (node.Next.Value, node.Next.Next);
+    }
+}
---
+++ DeleteNodeInALinkedList/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace DeleteNodeInALinkedList;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        var head = new Node(1, new (2, new (3, new (4, new (5)))));
+        new Solution().Delete(head.Next!.Next);
+        Assert.AreEqual(1, head.Value);
+        Assert.AreEqual(2, head.Next.Value);
+        Assert.AreEqual(4, head.Next.Next.Value);
+        Assert.AreEqual(5, head.Next.Next.Next.Value);
+        
+    }
+}
