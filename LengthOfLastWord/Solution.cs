f3fdadb602f6247223feb5ced5e63ea8a8b51360
Grigory Zhadko <gregzhadko@gmail.com>
2023-03-01 02:11:22 +0400

    LengthOfLastWord

---
+++ LengthOfLastWord/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LengthOfLastWord;
+
+public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        var i = s.Length - 1;
+        while(i >= 0 && s[i] == ' ')
+        {
+            i--;
+        }
+
+        var result = 0;
+        while(i >= 0 && s[i] != ' ')
+        {
+            result++;
+            i--;
+        }
+
+        return result;
+    }
+}
---
+++ LengthOfLastWord/Tests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace LengthOfLastWord;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.AreEqual(5, new Solution().LengthOfLastWord("Hello World"));
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        Assert.AreEqual(1, new Solution().LengthOfLastWord("a "));
+    }
+    
+    [Test]
+    public void Test3()
+    {
+        Assert.AreEqual(4, new Solution().LengthOfLastWord("   fly me   to   the moon  "));
+    }
+    
+    [Test]
+    public void Test4()
+    {
+        Assert.AreEqual(6, new Solution().LengthOfLastWord("luffy is still joyboy"));
+    }
+}
