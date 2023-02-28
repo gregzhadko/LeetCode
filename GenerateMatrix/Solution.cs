694086b838103315a140f1eff99693eb33115503
Grigory Zhadko <gregzhadko@gmail.com>
2023-03-01 02:39:12 +0400

    Generate Matrix

---
+++ GenerateMatrix/Solution.cs
@@ -0,0 +1,77 @@
+﻿namespace GenerateMatrix;
+
+public class Solution
+{
+    public int[][] GenerateMatrix(int n)
+    {
+        var result = new int[n][];
+        for (var i = 0; i < n; i++)
+        {
+            result[i] = new int[n];
+        }
+
+        var x = -1;
+        var y = 0;
+        var direction = Direction.Right;
+        var number = 1;
+        while (number <= n*n)
+        {
+            switch (direction)
+            {
+                case Direction.Right:
+                {
+                    x++;
+                    if (x == n - 1 || result[y][x + 1] != 0)
+                    {
+                        direction = Direction.Down;
+                    }
+
+                    break;
+                }
+                case Direction.Down:
+                {
+                    y++;
+                    if (y == n - 1 || result[y + 1][x] != 0)
+                    {
+                        direction = Direction.Left;
+                    }
+
+                    break;
+                }
+                case Direction.Left:
+                {
+                    x--;
+                    if (x == 0 || result[y][x - 1] != 0)
+                    {
+                        direction = Direction.Up;
+                    }
+
+                    break;
+                }
+                case Direction.Up:
+                {
+                    y--;
+                    if (y == 0 || result[y - 1][x] != 0)
+                    {
+                        direction = Direction.Right;
+                    }
+
+                    break;
+                }
+            }
+
+            result[y][x] = number;
+            number++;
+        }
+
+        return result;
+    }
+
+    public enum Direction
+    {
+        Left,
+        Right,
+        Up,
+        Down
+    }
+}
---
+++ GenerateMatrix/Tests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+
+namespace GenerateMatrix;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        var expected = new[]
+        {
+            new[] {1, 2, 3},
+            new[] {8, 9, 4},
+            new[] {7, 6, 5}
+        };
+        Assert.AreEqual(expected, new Solution().GenerateMatrix(3));
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        var expected = new[]
+        {
+            new[] {1, 2, 3, 4},
+            new[] {12, 13, 14, 5},
+            new[] {11, 16, 15, 6},
+            new[] {10, 9, 8, 7}
+        };
+        Assert.AreEqual(expected, new Solution().GenerateMatrix(4));
+    }
+    
+    [Test]
+    public void Test3()
+    {
+        var expected = new[]
+        {
+            new[] {1, 2, 3, 4, 5},
+            new[] {16, 17, 18, 19, 6},
+            new[] {15, 24, 25, 20, 7},
+            new[] {14, 23, 22, 21, 8},
+            new[] {13, 12, 11, 10, 9}
+        };
+        Assert.AreEqual(expected, new Solution().GenerateMatrix(5));
+    }
+    
+    [Test]
+    public void Test4()
+    {
+        var expected = new[]
+        {
+            new[] {1, 2, 3, 4, 5, 6},
+            new[] {20, 21, 22, 23, 24, 7},
+            new[] {19, 32, 33, 34, 25, 8},
+            new[] {18, 31, 36, 35, 26, 9},
+            new[] {17, 30, 29, 28, 27, 10},
+            new[] {16, 15, 14, 13, 12, 11}
+        };
+        Assert.AreEqual(expected, new Solution().GenerateMatrix(6));
+    }
+}
