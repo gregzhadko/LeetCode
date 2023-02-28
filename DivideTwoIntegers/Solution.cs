22acc953fc3876b633b746daa786fc5fddac68df
Grigory Zhadko <gregzhadko@gmail.com>
2023-03-01 02:01:15 +0400

    Divided two integers

---
+++ DivideTwoIntegers/Solution.cs
@@ -0,0 +1,57 @@
+﻿namespace DivideTwoIntegers;
+
+public class Solution
+{
+    public int Divide(int dividend, int divisor)
+    {
+        if (dividend == int.MinValue && divisor == -1)
+        {
+            return int.MaxValue;
+        }
+
+        if (divisor == 1)
+        {
+            return dividend;
+        }
+
+        if (dividend == 0)
+        {
+            return 0;
+        }
+
+        if (dividend == divisor)
+        {
+            return 1;
+        }
+
+        var isPositive = (divisor > 0 && dividend > 0) || (divisor < 0 && dividend < 0);
+
+        if (divisor > 0)
+        {
+            divisor = ~divisor + 1;
+        }
+
+        if (dividend > 0)
+        {
+            dividend = ~dividend + 1;
+        }
+
+
+        var result = 0;
+        while (dividend <= divisor)
+        {
+            var power = -1;
+            var value = divisor;
+            while (value >= int.MinValue / 2 && value + value >= dividend)
+            {
+                power += power;
+                value += value;
+            }
+
+            result += power;
+            dividend -= value;
+        }
+
+        return isPositive ? ~result + 1 : result;
+    }
+}
---
+++ DivideTwoIntegers/Tests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+
+namespace DivideTwoIntegers;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.AreEqual(3, new Solution().Divide(10, 3));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        Assert.AreEqual(5, new Solution().Divide(10, 2));
+    }
+    
+    [Test]
+    public void Test3()
+    {
+        Assert.AreEqual(1, new Solution().Divide(10, 10));
+    }
+    
+    [Test]
+    public void Test4()
+    {
+        Assert.AreEqual(0, new Solution().Divide(10, 100));
+    }
+    
+    [Test]
+    public void Test5()
+    {
+        Assert.AreEqual(-2, new Solution().Divide(7, -3));
+    }
+    
+    [Test]
+    public void Test6()
+    {
+        Assert.AreEqual(2147483647, new Solution().Divide(-2147483648, -1));
+    }
+    
+    [Test]
+    public void Test7()
+    {
+        Assert.AreEqual(2147483647, new Solution().Divide(2147483647, 1));
+    }
+    
+    [Test]
+    public void Test8()
+    {
+        Assert.AreEqual(1073741823, new Solution().Divide(2147483647, 2));
+    }
+    
+    [Test]
+    public void Test9()
+    {
+        Assert.AreEqual(-1073741824, new Solution().Divide(-2147483648, 2));
+    }
+}
