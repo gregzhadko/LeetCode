0b2a5a8b5efdb1eac0e4983a4601fbdf0c754db4
Grigory Zhadko <gregzhadko@gmail.com>
2023-03-07 01:37:25 +0400

    Candy

---
+++ Candy/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace Candy;
+
+public class Solution
+{
+    public int Candy(int[] ratings)
+    {
+        var result = new int[ratings.Length];
+        for (var i = 0; i < result.Length; i++)
+        {
+            result[i] = 1;
+        }
+
+        for (var i = 1; i < result.Length; i++)
+        {
+            if (ratings[i] > ratings[i - 1])
+            {
+                result[i] = result[i - 1] + 1;
+            }
+        }
+
+        for (var i = result.Length - 2; i >= 0; i--)
+        {
+            if (ratings[i] > ratings[i + 1] && result[i] <= result[i + 1])
+            {
+                result[i] = result[i + 1] + 1;
+            }
+        }
+
+        return result.Sum();
+    }
+}
---
+++ Candy/Tests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Candy;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.AreEqual(5, new Solution().Candy(new[] {1, 0, 2}));
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        Assert.AreEqual(4, new Solution().Candy(new[] {1, 2, 2}));
+    }
+    
+    [Test]
+    public void Test3()
+    {
+        Assert.AreEqual(11, new Solution().Candy(new[] {1, 3, 4, 5, 2}));
+    }
+    
+    
+}
