dc17201744f05f1a5243bf0ba84287573eb946f1
Grigory Zhadko <gregzhadko@gmail.com>
2023-03-08 02:23:39 +0400

    Merge sorted array

---
+++ MergeSortedArray/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace MergeSortedArray;
+
+public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        var i1 = m - 1;
+        var i2 = n - 1;
+
+        for (var i = m + n - 1; i >= 0; i--)
+        {
+            if (i1 < 0)
+            {
+                nums1[i] = nums2[i2];
+                i2--;
+            }
+            else if (i2 < 0)
+            {
+                nums1[i] = nums1[i1];
+                i1--;
+            }
+            else if (nums1[i1] > nums2[i2])
+            {
+                nums1[i] = nums1[i1];
+                i1--;
+            }
+            else
+            {
+                nums1[i] = nums2[i2];
+                i2--;
+            }
+        }
+    }
+}
---
+++ MergeSortedArray/Tests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace MergeSortedArray;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        var nums1 = new[] { 1, 2, 3, 0, 0, 0 };
+        var nums2 = new[] { 2, 5, 6 };
+        new Solution().Merge(nums1, 3, nums2, 3);
+        Assert.AreEqual(new[] { 1, 2, 2, 3, 5, 6 }, nums1);
+    }
+
+    [Test]
+    public void Test2()
+    {
+        var nums1 = new[] { 1 };
+        var nums2 = new[] { 0 };
+        new Solution().Merge(nums1, 1, nums2, 0);
+        Assert.AreEqual(new[] { 1 }, nums1);
+    }
+
+    [Test]
+    public void Test3()
+    {
+        var nums1 = new[] { 0 };
+        var nums2 = new[] { 1 };
+        new Solution().Merge(nums1, 0, nums2, 1);
+        Assert.AreEqual(new[] { 1 }, nums1);
+    }
+}
