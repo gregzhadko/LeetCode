2bad7d4d9642e188e93ff11f0d43a354a88c6cc6
Grigory Zhadko <gregzhadko@gmail.com>
2023-03-09 23:21:32 +0400

    N-ary tree level order traversal

---
+++ NaryTreeLevelOrderTraversal/Solution.cs
@@ -0,0 +1,37 @@
+﻿namespace NaryTreeLevelOrderTraversal;
+
+public class Solution
+{
+    public IList<IList<int>> LevelOrder(Node? root)
+    {
+        var result = new List<IList<int>>();
+        if (root == null)
+        {
+            return result;
+        }
+
+        var queue = new Queue<(Node, int)>();
+        queue.Enqueue((root, 0));
+        while (queue.Count > 0)
+        {
+            var (node, level) = queue.Dequeue();
+            if (result.Count == level)
+            {
+                result.Add(new List<int>());
+            }
+
+            result[level].Add(node.val);
+            if (node.children == null)
+            {
+                continue;
+            }
+
+            foreach (var nodeChild in node.children)
+            {
+                queue.Enqueue((nodeChild, level + 1));
+            }
+        }
+
+        return result;
+    }
+}
---
+++ NaryTreeLevelOrderTraversal/Tests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace NaryTreeLevelOrderTraversal;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        var root = new Node(1, new List<Node>
+        {
+            new(3, new List<Node>
+            {
+                new(5),
+                new(6)
+            }),
+            new(2),
+            new(4)
+        });
+        var expected = new List<IList<int>>
+        {
+            new List<int> { 1 },
+            new List<int> { 3, 2, 4 },
+            new List<int> { 5, 6 }
+        };
+        Assert.AreEqual(expected, new Solution().LevelOrder(root));
+    }
+}
