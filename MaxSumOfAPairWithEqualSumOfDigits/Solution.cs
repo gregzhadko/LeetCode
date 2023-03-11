dabd9fb77266078b943ba27959300843dfdec352
Grigory Zhadko <gregzhadko@gmail.com>
2023-03-12 03:11:10 +0400

    MaxSumOfAPairWithEqualSumOfDigits

---
+++ MaxSumOfAPairWithEqualSumOfDigits/Solution.cs
@@ -0,0 +1,58 @@
+﻿namespace MaxSumOfAPairWithEqualSumOfDigits;
+
+public class Solution
+{
+    public int MaximumSum(int[] nums)
+    {
+        var dict = new Dictionary<int, List<int>>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var num = nums[i];
+            var sum = GetDigitsSum(num);
+
+            if (dict.TryGetValue(sum, out var value))
+            {
+                if (value.Count == 2)
+                {
+                    if (value[0] < value[1])
+                    {
+                        if (value[0] < nums[i])
+                        {
+                            value[0] = nums[i];
+                        }
+                    }
+                    else
+                    {
+                        if (value[1] < nums[i])
+                        {
+                            value[1] = nums[i];
+                        }
+                    }
+                }
+                else
+                {
+                    value.Add(nums[i]);
+                }
+            }
+            else
+            {
+                dict.Add(sum, new(2) { nums[i] });
+            }
+        }
+
+        return dict.Select(d => d.Value).Where(v => v.Count == 2).Select(v => v[0] + v[1]).DefaultIfEmpty(-1).Max();
+    }
+
+    private static int GetDigitsSum(int num)
+    {
+        var sum = 0;
+        while (num / 10 > 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        sum += num;
+        return sum;
+    }
+}
---
+++ MaxSumOfAPairWithEqualSumOfDigits/Tests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace MaxSumOfAPairWithEqualSumOfDigits;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        var solution = new Solution();
+        var result = solution.MaximumSum(new[] { 18, 43, 36, 13, 7 });
+        Assert.AreEqual(54, result);
+    }
+
+    [Test]
+    public void Test2()
+    {
+        var solution = new Solution();
+        var result = solution.MaximumSum(new[] { 10, 12, 19, 14 });
+        Assert.AreEqual(-1, result);
+    }
+
+    [Test]
+    public void Test3()
+    {
+        var solution = new Solution();
+        var result = solution.MaximumSum(new[] { 229, 398, 269, 317, 420, 464, 491, 218, 439, 153, 482, 169, 411, 93, 147, 50, 347, 210, 251, 366, 401 });
+        Assert.AreEqual(973, result);
+    }
+}
